98b689cc1321e96768b86930d2a59d280ff34247
dvirBiton1 <dvir199719@gmail.com>
2022-05-10 14:17:20 -0400

    sad

--- Ex2.py
+++ Ex2.py
@@ -1,5 +1,5 @@
 from scapy.all import *
-
+from scapy.layers.inet import TCP
 
 ftpconns = {}
 faildftp = {}
@@ -30,3 +30,53 @@ def ftpAnalysis(p):
                 v = ftpconns[key].pop(port)
                 if v[0] in faildftp:
                     faildftp[v[0]] += 1
+                else:
+                    faildftp[v[0]] = 1
+sshconns = {}
+faildssh= {}
+threshold = 5000
+def sshAnalysis(p):
+    sip = p[IP].src
+    cip = p[IP].dst
+    key = "%s->%s" % (cip, sip)
+    port = p[TCP].dport
+    l = p[IP].len +14
+    if "F" in p[TCP].flags:
+        b = sshconns[key].pop(port)
+        b += 1
+        if b < threshold:
+            print("fuzzing!")
+    #         if key in faildssh:
+    #             faildssh[key] += 1
+    #         else:
+    #             faildssh[key] += 1
+    # else:
+    #     if not key in sshconns:
+    #         sshconns[key] = {}
+    #     if port in sshconns[key]:
+    #         sshconns[key][port] += 1
+    #     else:
+    #         if "S" in p[TCP].flags:
+    #             sshconns[key][port] = 1
+def analyzePacket(p):
+    if p.haslayer(TCP):
+        if (p[TCP].sport == 21 or p[TCP].dport == 21) and p.haslayer(Raw):
+            ftpAnalysis(p)
+        elif (p[TCP].sport == 22):
+            sshAnalysis(p)
+def printResults(openConns, failed, protocol):
+    print(f"open {protocol} connections:")
+    for conn in openConns:
+        c = openConns[conn]
+        if len(c) > 0:
+            print(conn)
+            for p in c:
+                print(f"\t port: {(p,c[p])} user:")
+    print(f"faild {protocol} logins:")
+    for f in failed:
+        print(f"\t {(f, failed[f])}")
+
+if __name__ == '__main__':
+    sniff(offline="ssh.pcapng", prn=analyzePacket)
+    # sniff(iface="enp0s3", filter="port 22", prn=analyzePacket)
+    # printResults(sshconns,faildssh, "SSH")
